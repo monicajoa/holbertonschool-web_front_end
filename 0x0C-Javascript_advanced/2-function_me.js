6885d90c7d787fe6808191ebc88aec9ce5306d69
monicajoa <mnortiz.ortiz@gmail.com>
2021-03-30 20:15:41 -0500

    task Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function welcomeMessage(fullName) {
+  return function () {
+    alert('Welcome' + ' ' + fullName);
+  };
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
+
+guillaume();
+alex();
+fred();
