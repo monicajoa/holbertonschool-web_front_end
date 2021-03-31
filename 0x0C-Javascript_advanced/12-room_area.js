cd5a9070ea045e4fbca2bfdb97f935480ed44eef
monicajoa <mnortiz.ortiz@gmail.com>
2021-03-30 20:50:50 -0500

    Add binding task

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea: function () {
+    return this.width * this.length;
+  },
+};
+
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
