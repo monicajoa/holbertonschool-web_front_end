75c044303ef37bbc2409b484c2f8e2c388d3e391
monicajoa <mnortiz.ortiz@gmail.com>
2021-03-30 20:58:29 -0500

    Add simple callback task

---
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function createElement(data) {
+  let paragraph = document.createElement('p');
+  paragraph.textContent = data.currentTarget.response;
+  document.body.appendChild(paragraph);
+}
+
+function queryWikipedia(callback) {
+  let request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*'
+  );
+  request.onload = callback;
+  request.send();
+}
+
+queryWikipedia(createElement);
